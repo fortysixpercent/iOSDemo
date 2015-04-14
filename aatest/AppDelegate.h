12bc98bf0151b1d86fc3ae6cf969ae9ceb37e7c9
Austin Alter <aalter@aalter-macpro.local>
2015-04-14 17:01:30 -0400

    Initial Commit

---
+++ aatest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aatest
+//
+//  Created by Austin Alter on 4/14/15.
+//  Copyright (c) 2015 Austin Alter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aatest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aatest
+//
+//  Created by Austin Alter on 4/14/15.
+//  Copyright (c) 2015 Austin Alter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
